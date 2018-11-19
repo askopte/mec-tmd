d93460b479ab45e2e9c525ce4deb858fbba85438
askoptembp <edgenovo@gmail.com>
2018-11-19 15:48:40 +0800

    Writing

--- drl-script/environment.py
+++ drl-script/environment.py
@@ -65,4 +65,117 @@ class Env:
             image_repr = np.zeros((self.pa.network_input_height, self.pa.network_input_width))
 
             ir_pt = 0
+
+            for i in xrange(self.pa.num_res): #res_slot repre
+                
+                image_repr[:, ir_pt: ir_pt + self.pa.res_slot] = self.machine.canvas[i, :, :]
+                ir_pt += self.pa.res_slot
+
+            for i in xrange(self.pa.nw_width): # nw_width
+
+                for j in xrange(self.pa.time_horizon):
+                    image_repr[i * self.pa.time_horizon + j, ir_pt : ir_pt + 1] = 
+                
+                ir_pt += 1
+                
+            for i in xrange(self.pa.job_width): # job_width
+
+                for j in xrange(self.pa.time_horizon):
+                    image_repr[i * self.pa.time_horizon + j, ir_pt : ir_pt + 3] = 
+                
+                ir_pt += 3
             
+            for i in xrange(self.pa.ambr_len):
+                image_repr[i, ir_pt:ir_pt + 1] = 
+
+            assert ir_pt == image_repr.shape[1]
+
+            return image_repr
+    
+    def get_reward(self):
+
+        reward = 0
+        for j in self.machine.running_job:
+            reward += self.pa.delay_penalty / float(j.len)
+
+        for j in self.job_slot.slot:
+            if j is not None:
+                reward += self.pa.hold_penalty / float(j.len)
+
+        return reward
+
+    def step(self, a, repeat=False):
+
+        status = None
+
+        done = False
+        reward = 0
+        info = None
+
+        if a == self.pa.num_nw:  # explicit void action
+            status = 'MoveOn'
+        elif self.job_slot.slot[a] is None:  # implicit void action
+            status = 'MoveOn'
+        else:
+
+    def reset(self):
+        self.seq_idx = 0
+        self.curr_time = 0
+
+        # initialize system
+        self.machine = Machine(self.pa)
+        self.job_slot = JobSlot(self.pa)
+        self.job_record = JobRecord()
+        self.extra_info = ExtraInfo(self.pa)
+        
+class Job:
+    def __init__(self,job_len, job_id, enter_time):
+        self.id = job_id
+        self.res = None
+        self.len = job_len
+        self.enter_time = enter_time
+        self.start_time = -1  # not being allocated
+        self.finish_time = -1
+
+
+class JobSlot:
+    def __init__(self, pa):
+        self.slot = [None] * pa.num_nw
+
+class JobRecord:
+    def __init__(self):
+        self.record = {}
+
+class Machine:
+    def __init__(self, pa):
+        self.num_res = pa.num_res
+        self.time_horizon = pa.time_horizon
+        self.res_slot = pa.res_slot
+
+        self.avbl_slot = np.ones((self.time_horizon, self.num_res)) * self.res_slot
+
+        self.running_job = []
+
+        # graphical representation
+        self.canvas = np.zeros((pa.num_res, pa.time_horizon, pa.res_slot))
+
+    def allocate_job(self, job, curr_time):
+
+        allocated = False
+
+    def time_proceed(self, curr_time):
+
+        self.avbl_slot[:-1, :] = self.avbl_slot[1:, :]
+        self.avbl_slot[-1, :] = self.res_slot
+
+        for job in self.running_job:
+
+            if job.finish_time <= curr_time:
+                self.running_job.remove(job)
+
+        # update graphical representation
+
+        self.canvas[:, :-1, :] = self.canvas[:, 1:, :]
+        self.canvas[:, -1, :] = 0
+    
+
--- drl-script/parameters.py
+++ drl-script/parameters.py
@@ -14,11 +14,14 @@ class Parameters:
         self.num_seq_per_batch = 10    # number of sequences to compute baseline
         self.episode_max_length = 200  # enforcing an artificial terminal
 
+        self.num_res = 2               # number of resources in the system
         self.num_nw = 200                # maximum allowed number of work in the queue
 
         self.time_horizon = 200         # number of time steps in the graph
         self.max_job_len = 150          # maximum duration of new jobs
-        self.res_slot = 10             # maximum number of available resource slots
+        self.res_slot = 16             # maximum number of available resource slots
+
+        self.ambr_len = 10             # LTE ambr prediction size
 
         self.max_track_since_new = 10  # track how many time steps since last new jobs
 
@@ -34,12 +37,14 @@ class Parameters:
         # graphical representation
         assert self.num_nw % self.time_horizon == 0  # such that it can be converted into an image
         assert self.job_num_cap % self.time_horizon == 0
+        assert self.ambr_len < self.time_horizon
+
         self.nw_width = int(math.ceil(self.num_nw / float(self.time_horizon)))
         self.job_width = int(math.ceil(self.job_num_cap / float(self.time_horizon)))
         self.network_input_height = self.time_horizon
         self.network_input_width = \
-            self.res_slot * 2 + \  
-            self.nw_width + self.job_width\
+            self.res_slot * self.num_res + \  
+            self.nw_width + self.job_width * 3\
             1  # for extra info, 1) time since last new job 2) LTE network infomation
         
     def compute_dependent_parameters(self):
@@ -49,6 +54,6 @@ class Parameters:
         self.job_width = int(math.ceil(self.job_num_cap / float(self.time_horizon)))
         self.network_input_height = self.time_horizon
         self.network_input_width = \
-            self.res_slot * 2 + \   
-            self.nw_width + self.job_width\
+            self.res_slot * self.num_res + \   
+            self.nw_width + self.job_width * 3\
             1  # for extra info, 1) time since last new job 2) LTE network infomation
