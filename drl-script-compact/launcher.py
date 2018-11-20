4d9206bc168ec723b773380aceb9e1e5e89db863
askopte <edgenovo@gmail.com>
2018-11-20 19:13:32 +0800

    using a compact approach rather than original DeepRM approach

---
+++ drl-script-compact/launcher.py
@@ -0,0 +1,10 @@
+import parameters
+
+def main():
+
+    pa = parameters.Parameters()
+
+    env = environment.Env(pa, end = end)
+
+if __name__ == '__main__':
+    main()
--- drl-script-compact/parameters.py
+++ drl-script-compact/parameters.py
@@ -46,6 +46,17 @@ class Parameters:
             self.res_slot * self.num_res + \  
             self.nw_width + self.job_width * 3\
             1  # for extra info, 1) time since last new job 2) LTE network infomation
+
+        self.network_output_dim = self.num_nw + 1  # + 1 for void action
+
+        self.delay_penalty = -1       # penalty for delaying things in the current work screen
+        self.hold_penalty = -1        # penalty for holding things in the new work screen
+        self.dismiss_penalty = -1     # penalty for missing a job because the queue is full
+
+        self.num_frames = 1           # number of frames to combine and process
+        self.lr_rate = 0.001          # learning rate
+        self.rms_rho = 0.9            # for rms prop
+        self.rms_eps = 1e-9           # for rms prop
         
     def compute_dependent_parameters(self):
         assert self.num_nw % self.time_horizon == 0  # such that it can be converted into an image
