1a788f309f2cd548aadcb9d3beb6f163a5bdee62
askoptembp <edgenovo@gmail.com>
2018-11-20 21:30:30 +0800

    writing

--- drl-script-compact/environment.py
+++ drl-script-compact/environment.py
@@ -40,57 +40,57 @@ class Env:
         self.job_record = JobRecord()
         self.extra_info = ExtraInfo(pa)
 
-        def generate_sequence_work(self, simu_len):
+    def generate_sequence_work(self, simu_len):
 
-        nw_len_seq = np.zeros(simu_len * 2, dtype=int)
+    nw_len_seq = np.zeros(simu_len * 2, dtype=int)
 
-        for i in range(simu_len):
+    for i in range(simu_len):
 
-            nw_len_seq[2 * i] = self.nw_dist()
+        nw_len_seq[2 * i] = self.nw_dist()
 
-            if np.random.rand() < self.pa.new_job_rate - 1:  # a new job comes
+        if np.random.rand() < self.pa.new_job_rate - 1:  # a new job comes
 
-                nw_len_seq[2 * i + 1] = self.nw_dist()
+            nw_len_seq[2 * i + 1] = self.nw_dist()
 
-        return nw_len_seq
+    return nw_len_seq
 
-        def get_new_job_from_seq(self, seq_no, seq_idx):
-        new_job = Job(job_len=self.nw_len_seqs[seq_no, seq_idx],
-                      job_id=len(self.job_record.record),
-                      enter_time=self.curr_time)
-        return new_job
+    def get_new_job_from_seq(self, seq_no, seq_idx):
+    new_job = Job(job_len=self.nw_len_seqs[seq_no, seq_idx],
+                  job_id=len(self.job_record.record),
+                  enter_time=self.curr_time)
+    return new_job
 
-        def observe(self):
+    def observe(self):
 
-            image_repr = np.zeros((self.pa.network_input_height, self.pa.network_input_width))
+        image_repr = np.zeros((self.pa.network_input_height, self.pa.network_input_width))
 
-            ir_pt = 0
+        ir_pt = 0
 
-            for i in xrange(self.pa.num_res): #res_slot repre
+        for i in xrange(self.pa.num_res): #res_slot repre
                 
-                image_repr[:, ir_pt: ir_pt + self.pa.res_slot] = self.machine.canvas[i, :, :]
-                ir_pt += self.pa.res_slot
+            image_repr[:, ir_pt: ir_pt + self.pa.res_slot] = self.machine.canvas[i, :, :]
+            ir_pt += self.pa.res_slot
 
-            for i in xrange(self.pa.nw_width): # nw_width
+        for i in xrange(self.pa.nw_width): # nw_width
 
-                for j in xrange(self.pa.time_horizon):
-                    image_repr[i * self.pa.time_horizon + j, ir_pt : ir_pt + 1] = 
+            for j in xrange(self.pa.time_horizon):
+                image_repr[i * self.pa.time_horizon + j, ir_pt : ir_pt + 1] = 
                 
-                ir_pt += 1
+            ir_pt += 1
                 
-            for i in xrange(self.pa.job_width): # job_width
+        for i in xrange(self.pa.job_width): # job_width
 
-                for j in xrange(self.pa.time_horizon):
-                    image_repr[i * self.pa.time_horizon + j, ir_pt : ir_pt + 3] = 
+            for j in xrange(self.pa.time_horizon):
+                image_repr[i * self.pa.time_horizon + j, ir_pt : ir_pt + 3] = 
                 
-                ir_pt += 3
+            ir_pt += 3
             
-            for i in xrange(self.pa.ambr_len):
-                image_repr[i, ir_pt:ir_pt + 1] = 
+        for i in xrange(self.pa.ambr_len):
+            image_repr[i, ir_pt:ir_pt + 1] = 
 
-            assert ir_pt == image_repr.shape[1]
+        assert ir_pt == image_repr.shape[1]
 
-            return image_repr
+        return image_repr
     
     def get_reward(self):
 
--- drl-script-compact/launcher.py
+++ drl-script-compact/launcher.py
@@ -1,10 +1,85 @@
 import parameters
+import os
+import time
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+import environment
+
+def get_traj(env, episode_max_length):
+
+    env.reset()
+    obs = []
+    acts = []
+    rews = []
+    info = []
+
+    ob = env.observe()
+
+    for _ in xrange(episode_max_length):
+        act = 
+        obs.append(ob)
+        acts.append(act)
+
+        ob,rew,done,info = env.step(act,repeat = True)
+
+        rews.append(rew)
+        
+        if done:break
+    
+    return {'reward': np.array(rews),
+            'ob': np.array(obs),
+            'action': np.array(acts),
+            'info': info
+            }
+        
+def concatenate_all_ob(trajs, pa):
+
+    timesteps_total = 0
+    for i in xrange(len(trajs)):
+        timesteps_total += len(trajs[i]['reward'])
+    
+    all_ob = np.zeros(
+        (timesteps_total, 1, pa.network_input_height, pa.network_input_width))
+
+    timesteps = 0
+    ffor i in xrange(len(trajs)):
+        for j in xrange(len(trajs[i]['reward'])):
+            all_ob[timesteps, 0, :, :] = trajs[i]['ob'][j]
+            timesteps += 1
+    
+    return all_ob
 
 def main():
 
     pa = parameters.Parameters()
 
     env = environment.Env(pa, end = end)
+
+    timer_start = time.time()
+
+    for iteration in xrange(pa.num_epochs):
+
+        all_oball_ob = []
+        all_action = []
+        all_adv = []
+        all_eprews = []
+        all_eplens = []
+
+        for ex in xrange(pa.num_ex):
+
+            trajs = []
+
+            for i in xrange(pa.num_seq_per_batch):
+                trajs = get_traj(env, pa.episode_max_length)
+                trajs.append(traj)
+            
+            env.seq_no = (env.seq_no + 1) % env.pa.num_ex
+
+            all_ob.append(concatenate_all_ob(trajs, pa))
+
+            rets = []
+
 
 if __name__ == '__main__':
     main()
