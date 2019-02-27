c45d400935ee95ee0724c2eab2ff1a0c91d068a2
askopte <edgenovo@gmail.com>
2019-02-27 16:01:31 +0800

    Working on parameters

--- drl-script-compact/parameters.py
+++ drl-script-compact/parameters.py
@@ -8,13 +8,13 @@ class Parameters:
         # self.output_filename = 'data/tmp'
         self.output_filename = 'D:\Code\data\mec-tmd\data_temp'
         self.num_epochs = 1001         # number of training epochs
-        self.simu_len = 100             # length of the busy cycle that repeats itself
+        self.simu_len = 200             # length of the busy cycle that repeats itself
         self.num_ex = 4                # number of sequences
 
         self.output_freq = 50          # interval for output and store parameters
 
         self.num_seq_per_batch = 10    # number of sequences to compute baseline
-        self.episode_max_length = 1000  # enforcing an artificial terminal
+        self.episode_max_length = 2000  # enforcing an artificial terminal
 
         self.num_res = 2               # number of resources in the system
         self.num_nw = 200                # maximum allowed number of work in the queue
